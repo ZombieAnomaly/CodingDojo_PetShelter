0363583f6400c3e6fda321aeb5e5d8c5d16d3b6f
ZombieAnomaly <kehran3@gmail.com>
2018-05-18 13:27:30 -0700

    fixed bug with html attribute

--- AngularApp/src/app/pet-details/pet-details.component.ts
+++ AngularApp/src/app/pet-details/pet-details.component.ts
@@ -34,7 +34,7 @@ export class PetDetailsComponent implements OnInit {
         this.getPetInfo();
       }
     })
-    document.getElementById('like').disabled = true;
+   (<HTMLInputElement> document.getElementById('like')).disabled = true;
   }
 
   adoptPet(id){
